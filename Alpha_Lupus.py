c4e72b41f094fffe608c16f5899e6e73d7ba0cb1
Kubabob <114347708+Kubabob@users.noreply.github.com>
2023-01-04 19:10:46 +0100

    Update Alpha_Lupus.py

--- Alpha_Lupus.py
+++ Alpha_Lupus.py
@@ -19,75 +19,32 @@ import discord
 from discord.ext import commands
 from discord.ext import tasks
 from dotenv import load_dotenv
+import aiohttp
+
 
 #ZAŁADOWANIE TOKENÓW Z .env
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
+
 #NADANIE BOTOWI INTENT(PRAWA)
 intents = discord.Intents.all()
+
 
 #UTWORZENIE BOTA
 bot = commands.Bot(command_prefix='!', intents=intents)
 bot.remove_command('help')
 
+
 #ID GILDII
 guild_id = 1058493924089548810
+
 
 #ID KANAŁÓW
 przywitanie_id = 1058535484344766474
 regulamin_id = 1058543121824227389
 admin_channel_id = 1059491305522221086
-
-#TWORZENIE REGULAMINU
-@bot.command()
-@commands.has_permissions(manage_channels=True)
-async def regulamin_maker(ctx):
-    await ctx.message.delete()
-    regulamin_message = await ctx.channel.send(regulamin)
-    await regulamin_message.add_reaction('✅')
-
-#ERROR DO TWORZENIA REGULAMINU
-@regulamin_maker.error
-async def regulamin_maker_error(ctx, error):
-    if isinstance(error, commands.MissingPermissions):
-        error_text = f'''Nie masz uprawnień do komendy "regulamin_maker"'''
-        await ctx.channel.send(error_text)
-    
-#GRA W PING PONGA
-@bot.command()
-async def ping(ctx):
-    await ctx.channel.send("pong :ping_pong:")
-
-#HELP @EVERYONE
-@bot.command()
-async def help(ctx):
-    help_embed = discord.Embed(color=discord.Colour.purple())
-
-    help_embed.set_author(name='!help')
-    help_embed.add_field(
-        name='!ping',
-        value='Grasz w pingla z botem',
-        )
-    
-
-    await ctx.channel.send(embed=help_embed)
-
-#HELP @ADMIN
-@bot.command(pass_context=True)
-async def help_admin(ctx):
-    admin_channel = bot.get_channel(admin_channel_id)
-
-    help_admin_embed = discord.Embed(color=discord.Colour.purple())
-
-    help_admin_embed.set_author(name='!help_admin')
-    help_admin_embed.add_field(
-        name='!regulamin_maker',
-        value='Służy do stworzenia regulaminu'
-        )
-
-    await admin_channel.send(embed=help_admin_embed)
 
 
 #ROZPOCZĘCIE DZIAŁANIA BOTA
@@ -118,6 +75,7 @@ async def on_member_join(member):
                 f'Zaznacz ✅ i baw się dobrze!'
                 )
 
+
 #USUNIĘCIE CZŁONKA
 @bot.event
 async def on_member_remove(member):
@@ -145,6 +103,106 @@ async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
     print(f'{str(payload.emoji)}')
     if str(payload.emoji) == '✅':
         await payload.member.add_roles(rola_czlonek)
+
+
+#HELP @EVERYONE
+@bot.command()
+async def help(ctx):
+    help_embed = discord.Embed(color=discord.Colour.purple())
+
+    help_embed.set_author(name='!help')
+    help_embed.add_field(
+        name='!ping',
+        value='Grasz w pingla z botem'
+        )
+    help_embed.add_field(
+        name='!dog',
+        value='Pokazuje pieska'
+        )
+    help_embed.add_field(
+        name='!cat',
+        value='Pokazuje kotka'
+    )
+    help_embed.add_field(
+        name='!echo',
+        value='Daj mi coś powiedzieć'
+    )
+    
+
+    await ctx.channel.send(embed=help_embed)
+
+
+#HELP @ADMIN
+@bot.command(pass_context=True)
+async def help_admin(ctx):
+    admin_channel = bot.get_channel(admin_channel_id)
+
+    help_admin_embed = discord.Embed(color=discord.Colour.purple())
+
+    help_admin_embed.set_author(name='!help_admin')
+    help_admin_embed.add_field(
+        name='!regulamin_maker',
+        value='Służy do stworzenia regulaminu'
+        )
+
+    await admin_channel.send(embed=help_admin_embed)
+
+
+#TWORZENIE REGULAMINU
+@bot.command()
+@commands.has_permissions(manage_channels=True)
+async def regulamin_maker(ctx):
+    await ctx.message.delete()
+    regulamin_message = await ctx.channel.send(regulamin)
+    await regulamin_message.add_reaction('✅')
+
+
+#ERROR DO TWORZENIA REGULAMINU
+@regulamin_maker.error
+async def regulamin_maker_error(ctx, error):
+    if isinstance(error, commands.MissingPermissions):
+        error_text = f'''Nie masz uprawnień do komendy "regulamin_maker"'''
+        await ctx.channel.send(error_text)
+
+
+'''@bot.command(pass_context = True)
+async def clear(ctx):
+    print(f'{ctx.channel_id}')
+    await ctx.channel.delete_messages(ctx.channel.messages)
+    '''
+
+#GRA W PING PONGA
+@bot.command()
+async def ping(ctx):
+    await ctx.channel.send("pong :ping_pong:")
+
+
+#BOT PISZE TO CO TY
+@bot.command()
+async def echo(ctx, text):
+    await ctx.channel.send(text)
+
+
+#BOT WYSWIETLA LOSOWEGO KOTA
+@bot.command()
+async def cat(ctx):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://aws.random.cat/meow') as r:
+            if r.status == 200:
+                js = await r.json()
+                await ctx.channel.send(js['file'])
+
+
+#BOT WYSWIETLA LOSOWEGO PSA
+@bot.command()
+async def dog(ctx):
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://random.dog/woof.json') as r:
+            if r.status == 200:
+                js = await r.json()
+                await ctx.channel.send(js['url'])
+
+#cute - animal
 
 
 #URUCHOMIENIE BOTA
